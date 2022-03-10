f8d8b139f3364e22614417950dbc97a064d01f56
Aswin singh <aswnbit@gmail.com>
2022-03-10 18:32:49 +0530

    commit project

---
+++ demo1/src/main/java/com/example/demo/Response.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Response {
+	
+	@GetMapping("/appl/{name}")
+	
+	 ResponseEntity<String> getmessage(){
+		 
+		String name="";
+		if(name.equals("abcd")) {
+			
+			return new ResponseEntity<>("not good "+name,HttpStatus.BAD_REQUEST);	
+		}
+		 	 
+		 return new ResponseEntity<>("welcome to new page"+name,HttpStatus.OK);
+		 
+		 
+	 }
+	
+	
+	
+	
+
+}
